a9068419b2152a30f55af1bd1ed0fbcf1c9036c6
Zachary <zbarryte@mit.edu>
2016-04-04 02:20:45 -0400

    player now collides with walls

--- src/edu/mit/d54/plugins/hacman/HacManPlugin.java
+++ src/edu/mit/d54/plugins/hacman/HacManPlugin.java
@@ -20,19 +20,47 @@ public class HacManPlugin extends DisplayPlugin implements ArcadeListener {
 	private java.util.ArrayList<Transform> physicsTransfoms;
 	private long timeSinceLastUpdate;
 
+	private java.util.HashMap<Transform,java.util.ArrayList<Transform>> collisionMap;
+
+	private java.util.ArrayList<Transform> walls;
+
 	public HacManPlugin(Display2D display, double framerate) throws IOException {
 
 		super(display, framerate);
 
 		controller = ArcadeController.getInstance();
+
+
+		walls = new java.util.ArrayList<Transform>();
+
+		// TODO: generate walls from map
+		Transform wall = new Transform();
+		wall.x = 6;
+		wall.y = 8;
+		walls.add(wall);
+
+		// create the player
 		player = new Player();
 
+		// TODO: position player based on spawn point of map
+		player.transform.x = 4;
+		player.transform.y = 8;
+
+		// our collision system requires only moving objects to map to things with which they can collide
+		collisionMap = new java.util.HashMap<Transform,java.util.ArrayList<Transform>>();
+
+		java.util.ArrayList<Transform> playerColliders = new java.util.ArrayList<Transform>();
+		for (Transform transform : walls) {
+			playerColliders.add(transform);
+		}
+		collisionMap.put(player.transform,playerColliders);
+
+		// all moving objects should be added to the physics list, so they can be moved
 		physicsTransfoms = new java.util.ArrayList<Transform>();
 		physicsTransfoms.add(player.transform);
 
+		// we need to set up our initial time here, so that the first update doesn't get sad
 		timeSinceLastUpdate = System.nanoTime();
-
-		MovePlayer(1,0);
 	}
 
 	@Override
@@ -73,32 +101,14 @@ public class HacManPlugin extends DisplayPlugin implements ArcadeListener {
 		Display2D d = getDisplay();
 
 		// draw map
+		for (Transform wall : walls) {
+			d.setPixelHSB((int)wall.x,(int)wall.y,0.65f,1,1);
+		}
 		// draw pellets
 		// draw ghosts
 		// draw player
 		d.setPixelHSB((int)player.transform.x,(int)player.transform.y,0.15f,1,1);
 		// draw fruit(?)
-		
-		// for (int x=0; x<d.getWidth(); x++)
-		// {
-		// 	for (int y=0; y<d.getHeight(); y++)
-		// 	{
-		// 		if (x<5 && y<7)
-		// 		{
-		// 			if ((x+y)%2==0)
-		// 				d.setPixelHSB(x,y,0.666f,1,1);
-		// 			else
-		// 				d.setPixelHSB(x,y,0,0,1);
-		// 		}
-		// 		else
-		// 		{
-		// 			if (x%2==0)
-		// 				d.setPixelHSB(x,y,0,1,1);
-		// 			else
-		// 				d.setPixelHSB(x,y,0,0,1);
-		// 		}
-		// 	}
-		// }
 	}
 
 	private void MovePlayer(float dx, float dy) {
@@ -121,9 +131,7 @@ public class HacManPlugin extends DisplayPlugin implements ArcadeListener {
 			float xNew = transform.x + dx;
 			float yNew = transform.y + dy;
 
-			// check collisions to see if it can move
-
-			// wrap around the bo
+			// wrap around the board
 			Display2D d=getDisplay();
 			float width = d.getWidth();
 			float height = d.getHeight();
@@ -132,9 +140,26 @@ public class HacManPlugin extends DisplayPlugin implements ArcadeListener {
 			if (yNew >= height) {yNew -= height;}
 			if (yNew < 0) {yNew += height;}
 
+			// we only want to move if we won't collide with something solid
+			boolean canMove = true;
+			java.util.ArrayList<Transform> colliders = collisionMap.get(transform);
+			if (colliders != null) {
+				for (Transform collider : colliders) {
+					if ((int)xNew == (int)collider.x && (int)yNew == (int)collider.y) {
+
+						canMove = false;
+						break;
+
+					}
+				}
+			}
+
+			if (!canMove) {continue;}
+
 			// do move
 			transform.x = xNew;
 			transform.y = yNew;
+			
 		}
 	}
 
